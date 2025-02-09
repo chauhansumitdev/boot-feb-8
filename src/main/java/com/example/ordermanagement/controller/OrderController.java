094661eb1c7fad6db29c70f049fcf157cd92324f
chauhansumit <chauhansumit9009@gmail.com>
2025-02-09 21:59:31 +0530

    task/order-endpoint-implemented

---
+++ src/main/java/com/example/ordermanagement/controller/OrderController.java
@@ -0,0 +1,45 @@
+package com.example.ordermanagement.controller;
+
+
+import com.example.ordermanagement.entity.Order;
+import com.example.ordermanagement.service.OrderService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+
+    @PostMapping("/v1/create")
+    public ResponseEntity<Order> createOrder(@RequestBody Order order){
+        return new ResponseEntity<>(orderService.createOrder(order), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/v1/read/{id}")
+    public ResponseEntity<Order> readOrder(@PathVariable UUID id){
+        return new ResponseEntity<>(orderService.readOrder(id), HttpStatus.OK);
+    }
+
+
+    @PutMapping("/v1/update/{id")
+    public ResponseEntity<Order> updateOrder(@PathVariable UUID id, @RequestBody Order order){
+        return new ResponseEntity<>(orderService.updateOrder(id, order), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/v1/delete/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable UUID id){
+        orderService.deleteOrder(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/example/ordermanagement/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.example.ordermanagement.service;
+
+
+import com.example.ordermanagement.entity.Order;
+import com.example.ordermanagement.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+
+    public Order createOrder(Order order){
+        return orderRepository.save(order);
+    }
+
+    public Order readOrder(UUID id){
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    public Order updateOrder(UUID id, Order order){
+        Order existingOrder = orderRepository.findById(id).orElse(null);
+
+        if(existingOrder != null){
+            existingOrder.setOrderNumber(order.getOrderNumber());
+            existingOrder.setStatus(order.getStatus());
+            existingOrder.setCustomer(order.getCustomer());
+        }
+
+        return existingOrder;
+    }
+
+    public void deleteOrder(UUID id){
+        orderRepository.deleteById(id);
+    }
+}
